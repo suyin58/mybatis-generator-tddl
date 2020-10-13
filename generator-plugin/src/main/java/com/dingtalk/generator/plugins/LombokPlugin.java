d45ff282ec9cc0985859bbc40bf736257aca559f
zhenyin.szy <zhenyin.szy@alibaba-inc.com>
2020-10-13 19:32:03 +0800

    add lombok,comment plugin

---
+++ generator-plugin/src/main/java/com/dingtalk/generator/plugins/LombokPlugin.java
@@ -0,0 +1,217 @@
+package com.dingtalk.generator.plugins;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Lombok
+ * @author suyin
+ */
+public class LombokPlugin extends PluginAdapter {
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+
+    private final Collection<Annotations> annotations;
+    /**
+     * LombokPlugin constructor
+     */
+    public LombokPlugin() {
+
+        annotations = new LinkedHashSet<Annotations>(Annotations.values().length);
+    }
+
+
+    /**
+     * Intercepts base record class generation
+     *
+     * @param topLevelClass
+     *            the generated base record class
+     * @param introspectedTable
+     *            The class containing information about the table as
+     *            introspected from the database
+     * @return always true
+     */
+    @Override
+    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass,
+                                                 IntrospectedTable introspectedTable) {
+        addDataAnnotation(topLevelClass);
+        return true;
+    }
+
+    /**
+     * Intercepts primary key class generation
+     *
+     * @param topLevelClass
+     *            the generated primary key class
+     * @param introspectedTable
+     *            The class containing information about the table as
+     *            introspected from the database
+     * @return always true
+     */
+    @Override
+    public boolean modelPrimaryKeyClassGenerated(TopLevelClass topLevelClass,
+                                                 IntrospectedTable introspectedTable) {
+        addDataAnnotation(topLevelClass);
+        return true;
+    }
+
+    /**
+     * Intercepts "record with blob" class generation
+     *
+     * @param topLevelClass
+     *            the generated record with BLOBs class
+     * @param introspectedTable
+     *            The class containing information about the table as
+     *            introspected from the database
+     * @return always true
+     */
+    @Override
+    public boolean modelRecordWithBLOBsClassGenerated(
+            TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        addDataAnnotation(topLevelClass);
+        return true;
+    }
+
+    /**
+     * Prevents all getters from being generated.
+     * See SimpleModelGenerator
+     *
+     * @param method
+     *            the getter, or accessor, method generated for the specified
+     *            column
+     * @param topLevelClass
+     *            the partially implemented model class
+     * @param introspectedColumn
+     *            The class containing information about the column related
+     *            to this field as introspected from the database
+     * @param introspectedTable
+     *            The class containing information about the table as
+     *            introspected from the database
+     * @param modelClassType
+     *            the type of class that the field is generated for
+     */
+    @Override
+    public boolean modelGetterMethodGenerated(Method method,
+                                              TopLevelClass topLevelClass,
+                                              IntrospectedColumn introspectedColumn,
+                                              IntrospectedTable introspectedTable,
+                                              ModelClassType modelClassType) {
+        return false;
+    }
+
+    /**
+     * Prevents all setters from being generated
+     * See SimpleModelGenerator
+     *
+     * @param method
+     *            the setter, or mutator, method generated for the specified
+     *            column
+     * @param topLevelClass
+     *            the partially implemented model class
+     * @param introspectedColumn
+     *            The class containing information about the column related
+     *            to this field as introspected from the database
+     * @param introspectedTable
+     *            The class containing information about the table as
+     *            introspected from the database
+     * @param modelClassType
+     *            the type of class that the field is generated for
+     * @return always false
+     */
+    @Override
+    public boolean modelSetterMethodGenerated(Method method,
+                                              TopLevelClass topLevelClass,
+                                              IntrospectedColumn introspectedColumn,
+                                              IntrospectedTable introspectedTable,
+                                              ModelClassType modelClassType) {
+        return false;
+    }
+
+    /**
+     * Adds the lombok annotations' imports and annotations to the class
+     *
+     * @param topLevelClass
+     *            the partially implemented model class
+     */
+    private void addDataAnnotation(TopLevelClass topLevelClass) {
+        for (Annotations annotation : annotations) {
+            topLevelClass.addImportedType(annotation.javaType);
+            topLevelClass.addAnnotation(annotation.name);
+        }
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+        super.setProperties(properties);
+
+        //@Data is default annotation
+        annotations.add(Annotations.DATA);
+
+        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+            boolean isEnable = Boolean.parseBoolean(entry.getValue().toString());
+
+            if (isEnable) {
+                String paramName = entry.getKey().toString().trim();
+                Annotations annotation = Annotations.getValueOf(paramName);
+                if (annotation != null) {
+                    annotations.add(annotation);
+                    annotations.addAll(Annotations.getDependencies(annotation));
+                }
+            }
+        }
+    }
+
+
+    private enum Annotations {
+        DATA("data", "@Data", "lombok.Data"),
+        BUILDER("builder", "@Builder", "lombok.Builder"),
+        SUPER_BUILDER("superBuilder", "@SuperBuilder", "lombok.experimental.SuperBuilder"),
+        ALL_ARGS_CONSTRUCTOR("allArgsConstructor", "@AllArgsConstructor", "lombok.AllArgsConstructor"),
+        NO_ARGS_CONSTRUCTOR("noArgsConstructor", "@NoArgsConstructor", "lombok.NoArgsConstructor"),
+        TO_STRING("toString", "@ToString", "lombok.ToString");
+
+
+        private final String paramName;
+        private final String name;
+        private final FullyQualifiedJavaType javaType;
+
+
+        Annotations(String paramName, String name, String className) {
+            this.paramName = paramName;
+            this.name = name;
+            this.javaType = new FullyQualifiedJavaType(className);
+        }
+
+        private static Annotations getValueOf(String paramName) {
+            for (Annotations annotation : Annotations.values()) {
+                if (String.CASE_INSENSITIVE_ORDER.compare(paramName, annotation.paramName) == 0) {
+                    return annotation;
+                }
+            }
+
+            return null;
+        }
+
+        private static Collection<Annotations> getDependencies(Annotations annotation) {
+            if (annotation == ALL_ARGS_CONSTRUCTOR) {
+                return Collections.singleton(NO_ARGS_CONSTRUCTOR);
+            } else {
+                return Collections.emptyList();
+            }
+        }
+    }
+}
---
+++ generator-plugin/src/main/java/com/dingtalk/generator/plugins/SelectByUniqueKeyPlugin.java
@@ -0,0 +1,8 @@
+package com.dingtalk.generator.plugins;
+
+/**
+ * select by uniqueKey, should assign unique key name
+ * @author suyin
+ */
+public class SelectByUniqueKeyPlugin {
+}
